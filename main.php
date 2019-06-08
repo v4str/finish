8cc23f9768118452c37159d49f42a416b572694f
v4str <igorekmelnik@gmail.com>
2019-06-09 02:51:24 +0300

    comments page

---
+++ main.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="todo.js"></script>
+</head>
+<body>
+<div class="wrapper" id="11">
+    <div class="header" >
+        <h1>Список дел</h1>
+        <div>
+            <input type="text" placeholder="Введите новое дело..." id="l2" name="addMessage">
+        </div>
+
+        <input id="SHOW" id="button" class="addBtn" type="submit" onclick="add()" value="Добавить">
+    </div>
+    <ul id="task">
+
+    </ul>
+
+</div>
+</body>
+</html>
+
