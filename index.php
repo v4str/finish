6919f0108077b467f9a9e1b7325098f0a7a3db62
v4str <igorekmelnik@gmail.com>
2019-06-09 02:42:10 +0300

    start page file

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<title>Главная страница</title>
+</head>
+<body>
+	<div  align="center" id="header">
+	<h1>ToDoList</h1>
+	</div>
+	<div  align="center" id="p1">
+	<div >
+		<div class="col" align="center">
+		<form action="check.php" method="POST">
+		<div  id="index">
+			<p>
+				<h2>Логин <input type="text"  widht= auto align="right" name="login"></h2>
+    		</p>
+    		<p>
+				<h2>Пароль <input type="password" widht= auto align="right" name="password"></h2>
+			</p>
+			<p>
+				<button type="submit">Войти</button>
+				<a href="reg.php"><button type="button" >Регистрация</button></a>
+			</p>   		
+  		</div>
+  		</div>
+	</div>
+	</div>
+
+</form>
+	
+</body>
+</html>
