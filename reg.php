41ddcb9cf1a3a5f2e56a157aa7b5f1b31e5e1cae
v4str <igorekmelnik@gmail.com>
2019-06-09 02:46:43 +0300

    registration php file

---
+++ reg.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	
+	<title>Регистрация</title>
+</head>
+
+<body>
+	<form action="save_user.php" align="center" method="POST">
+	<div  align="center" id="header">
+	
+	</div>
+	<div id="index">
+	<p>
+		<h2 >Введите ваш логин <input type="text"  widht= auto align="center"name="login"></h2>
+	</p>
+	<p>
+		<h2>Введите ваш пароль<input type="password"  widht= auto align="center"name="password"></h2>
+	</p>
+	<p>
+		<h2>Повторите ваш пароль<input type="password" widht= auto align="center"name="password1"></h2>
+	</p>
+	<p>
+		<button type="submit" >Зарегистрироваться</button></a>
+		<a href="index.php"><button type="button" align ='center'">Назад</button></a>
+	</p>
+	</div>
+	</form>
+</body>
+</html>
